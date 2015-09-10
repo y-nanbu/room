a39b072f2d2a2f12df9b912e6c724ce7dec90e3f
c185037 <h-hasegawa@tads.co.jp>
2015-09-10 16:13:29 +0900

    ログイン後のヘッダにログインユーザの名前を表示するように機能追加した

--- src/main/java/jp/co/tads/room/app/controller/RoomController.java
+++ src/main/java/jp/co/tads/room/app/controller/RoomController.java
@@ -1,10 +1,14 @@
 package jp.co.tads.room.app.controller;
 
 import jp.co.tads.room.app.controller.base.ControllerBase;
+import jp.co.tads.room.infra.security.LoginUserDetails;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * ルーム機能のコントローラクラスです。
@@ -19,8 +23,11 @@ public class RoomController extends ControllerBase {
      * ルーム画面を表示します。
      */
     @RequestMapping(method = RequestMethod.GET)
-    String room(Model model) {
-        setPageTitle(model, "");
+    String room(Model model, HttpSession session) {
+        SecurityContext sc = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
+        LoginUserDetails loginUserDetails = (LoginUserDetails) sc.getAuthentication().getPrincipal();
+
+        setPageTitle(model, loginUserDetails.getUser().getName());
         return "room/room";
     }
 }
